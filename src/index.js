f111058c673c8468b3feb613d595ee80c88d1e52
spiderVS <fuzzy003@gmail.com>
2021-03-09 19:20:16 +0300

    fix: add check nullparameters

--- src/index.js
+++ src/index.js
@@ -2,17 +2,16 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-
-  let sortedTowel = [];
-  for (let i = 0; i < matrix.length; i++) {	
-		if (i % 2 == 1) {
-			for (let k = matrix[i].length-1; k >= 0; k--) {
-			  sortedTowel = sortedTowel.concat(matrix[i][k]);
-			}		
-		} else {
-				sortedTowel = sortedTowel.concat(matrix[i]);
-			}    	
-  }
-  
+if (!matrix) return [];	
+let sortedTowel = [];
+for (let i = 0; i < matrix.length; i++) {	
+	if (i % 2 == 1) {
+		for (let k = matrix[i].length-1; k >= 0; k--) {
+			sortedTowel = sortedTowel.concat(matrix[i][k]);
+		}		
+	} else {
+			sortedTowel = sortedTowel.concat(matrix[i]);
+		}    	
+}
 	return sortedTowel;
 }
